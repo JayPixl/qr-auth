7ba3f57d789760622dff6b69d3dfbf79806126bf
Joshua Lawrence, Jr <jaypixl@protonmail.com>
2023-10-19 17:09:14 -0400

    ReadME and View Source Button

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,16 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className='max-w-screen overflow-x-hidden overscroll-none'>
+        <a href='https://github.com/JayPixl/qr-auth' className="fixed bottom-2 left-2 z-50">
+          <div
+            className="rounded-full py-2 px-4 font-bold bg-zinc-900 text-zinc-200 flex flex-row items-center opacity-50 hover:opacity-90 transition"
+          >
+            <div className="font-mono mr-2">View Page Source</div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
+            </svg>
+          </div>
+        </a>
         <Navbar />
         {children}
       </body>
